57aa0d63d71ca9e24073b2784994ab9a8f4ffe39
Kadir Tastepe <70476992+kadirtastepe@users.noreply.github.com>
2023-02-21 23:04:06 +0100

    Update expofit-macro.cc

--- expofit-macro.cc
+++ expofit-macro.cc
@@ -1,22 +1,23 @@
+// This program fits data to exponential function and writes the fitted equation on the plot with variables.
 #include "TStyle.h"
 #include "TLatex.h"
 
 {
 gStyle->SetOptStat(0);
-TFile f("100.root");
+TFile f("file.root");
 gStyle->SetTextFont(132);
 
 TCanvas c1("c1","Histograms" ,1200,1200);
-TH1F *mmass = (TH1F*)f.Get("hs_mmass000");
+TH1F *mmass = (TH1F*)f.Get("muon_mass");
 
 //mmass->Scale(1.0 / mmass->Integral()); //Normalize
 
 mmass->SetMinimum(0.1); //Necessary for log-scale
 
-TF1 *fit = new TF1("fit", "expo", 91.4, 115);
+//TF1 *fit = new TF1("fit", "TMath::Exp([0]+[1]*x)", 0, 120); //User defined function
+TF1 *fit = new TF1("fit", "expo", 0, 120); //Predefined function
 
-Double_t par[9];
-mmass-> Fit("fit","R");
+mmass-> Fit("fit","R"); // R->Range (Fits on defined range)
 
 //GETPARAMETERS
 double A = fit->GetParameter(0);
@@ -37,20 +38,18 @@ mmass->Draw();
 Double_t lambda = 0.5;
 
 
-char buf[100];
-sprintf(buf, "f(x) = e^{%2.2f x+%2.2f}",B,A);
-
-
+char eq[100];
+sprintf(eq, "f(x) = e^{%2.2f + %2.2f x}",A,B);
 
    TLatex *text = new TLatex();
    text->SetNDC();
    text->SetTextSize(0.055);
-   text->DrawLatex(0.4, 0.7, buf);
+   text->DrawLatex(0.4, 0.7, eq);
 
    text->SetTextSize(0.050);
    c1.Modified(); c1.Update();
 
-c1.SaveAs("Fits/100100fit.png");
+c1.SaveAs("Expo-Fit.png");
 
 } 
 
